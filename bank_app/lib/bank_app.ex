bff5f87c05701975f5e74aaea5bd92d560860a91
Christian Gerard <102694030+christian-gerard@users.noreply.github.com>
2024-12-14 18:12:42 -0700

    Updating bank Stuff

---
+++ bank_app/lib/bank_app.ex
@@ -0,0 +1,21 @@
+defmodule BankApp do
+  @moduledoc """
+  Documentation for `BankApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BankApp.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "TESTING"
+  end
+
+end
+
+BankApp.hello()
